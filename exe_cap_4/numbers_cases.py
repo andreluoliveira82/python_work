5e6f1095edd84157be62f778fd2ac1ac6444a56b
Andre Oliveira <88242664+andreluoliveira82@users.noreply.github.com>
2023-10-03 16:12:24 -0300

    Adicionando mais exercicios

--- exe_cap_4/numbers_cases.py
+++ exe_cap_4/numbers_cases.py
@@ -51,3 +51,92 @@ for n in range(1, 11):
 
 #Imprimindo a lista de quadrados
 print(f'\nA lista dos números quadrdados contém {len(squares)} elementos:\n{squares}')
+
+#-------------------------------------------------------------------------------
+# Estatísticas simples com uma lista de numeros:
+
+numbers = list(range(0,1000001,10))
+print(f'\nEstatísticas simples com uma lista de numeros:')
+print(f'\nA lista contém {len(numbers)} elementos\n')
+#print(numbers)
+print(f'Mínimo: {min(numbers)}')
+print(f'Máximo: {max(numbers)}')
+print(f'Soma: {sum  (numbers)}')
+
+#-------------------------------------------------------------------------------
+#André Oliveira
+#03-10-2023 noite
+#Enquanto assistia a Corinthians e Fortaleza jogando pela Copa Sulamericana valendo um vaga na final
+#----------------------------------------------------------------------------------------------------
+#Exercicio 4.3 - contando de 1 a 20
+numeros =[]
+for n in range(1,21):
+    numeros.append(n)
+
+#exibindo o resultado
+print(f'\nExercicio 4.3 - Lista dos números de 1 a 20:\n{numeros}') 
+
+#----------------------------------------------------------------------------------------------------
+#Exercicio 4.4 - Lista dos numeros de 1 a 1000000
+
+#Criando a lista de numeros de 1 a 1000000
+lista_milhao = list(range(1,1000001))
+
+#Imprimindo cada numero da lista por meio do loop for
+#for n in lista_milhao:
+    #print(n)
+
+#exibindo uma mensagem commum a todos os elementos
+print(f'\nExercicio 4.4\nA lista do milhão contém {len(lista_milhao)} elementos')
+
+#----------------------------------------------------------------------------------------------------
+#Exercicio 4.5 - somando um milhão
+#Criando a lista de numeros de 1 a 1000000
+print('\nExercicio 4.5 - somando um milhão')
+print(f'Mínimo: {min(lista_milhao)}')
+print(f'Máximo: {max(lista_milhao)}')
+print(f'Soma dos elementos: {sum(lista_milhao)}')
+
+#----------------------------------------------------------------------------------------------------
+#Exercicio 4.6 - Números Impres até 20
+impares = list(range(1,20,2))
+
+print('\nExercicio 4.6 - Números Impres até 20')
+#imprimindo a lsita 
+print(f'Lista dos numeros impares entre um 1 e 20\n{impares}')
+
+#----------------------------------------------------------------------------------------------------
+#Exercicio 4.7 - Múltiplos dos números de 3, entre 3 e 30
+multiplos_tres = list(range(3,31,3))
+
+print('\nExercicio 4.7 - Múltiplos dos números de 3, entre 3 e 30\n')
+
+for n in multiplos_tres:
+    print(n)
+
+#imprimindo a uma mensagem comum a toda a lista
+print(f'Lista dos numeros múltiplos de tres entre um 3 e 30\n{multiplos_tres}\n Contém {len(multiplos_tres)} elementos')
+
+#----------------------------------------------------------------------------------------------------
+#Exercicio 4.8 - Cubos dos 10 primeiros numeros
+
+cubos_dez = []
+soma_cubos=0
+
+print('\nExercicio 4.8 - Cubos dos 10 primeiros numeros\n')
+
+#Exibindo o valor cubo de cada numero
+for n in range(1,11):
+    
+    print(f'{n}³ = {n**3}')
+    cubos_dez.append({n ** 3}) #\gurdando os cubos em uma lista para usar depois / isto não fazia parte do execicio.
+    soma_cubos = soma_cubos + (n ** 3) # Somando os cubos
+
+#usando os cubos guardados em memoria
+print(f'A soma dos cubos dos 10 primeiros numeros inteiros positivos é:\n {soma_cubos}')
+
+#----------------------------------------------------------------------------------------------------
+#Exercicio 4.9 - Cube Comprehension
+cubos_dez = [n ** 3  for n in range(1,10)]
+
+print(f'\n List Comprehension dos cubos:\n{cubos_dez}')
